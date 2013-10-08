4b02087995d78bb496239e9e8e7b731ebb2aec07
Thales Costa <thalesfc@gmail.com>
2013-10-08 19:25:01 -0300

    Implement the weighted TF-IDF

--- cbf-assignment/src/main/java/org/grouplens/mooc/cbf/TFIDFItemScorer.java
+++ cbf-assignment/src/main/java/org/grouplens/mooc/cbf/TFIDFItemScorer.java
@@ -17,93 +17,127 @@ import java.util.List;
  * @author <a href="http://www.grouplens.org">GroupLens Research</a>
  */
 public class TFIDFItemScorer extends AbstractItemScorer {
-    private final UserEventDAO dao;
-    private final TFIDFModel model;
+	private final UserEventDAO dao;
+	private final TFIDFModel model;
 
-    /**
-     * Construct a new item scorer.  LensKit's dependency injector will call this constructor and
-     * provide the appropriate parameters.
-     *
-     * @param dao The user-event DAO, so we can fetch a user's ratings when scoring items for them.
-     * @param m   The precomputed model containing the item tag vectors.
-     */
-    @Inject
-    public TFIDFItemScorer(UserEventDAO dao, TFIDFModel m) {
-        this.dao = dao;
-        model = m;
-    }
+	/**
+	 * Construct a new item scorer.  LensKit's dependency injector will call this constructor and
+	 * provide the appropriate parameters.
+	 *
+	 * @param dao The user-event DAO, so we can fetch a user's ratings when scoring items for them.
+	 * @param m   The precomputed model containing the item tag vectors.
+	 */
+	@Inject
+	public TFIDFItemScorer(UserEventDAO dao, TFIDFModel m) {
+		this.dao = dao;
+		model = m;
+	}
 
-    /**
-     * Generate item scores personalized for a particular user.  For the TFIDF scorer, this will
-     * prepare a user profile and compare it to item tag vectors to produce the score.
-     *
-     * @param user   The user to score for.
-     * @param output The output vector.  The contract of this method is that the caller creates a
-     *               vector whose possible keys are all items that should be scored; this method
-     *               fills in the scores.
-     */
-    @Override
-    public void score(long user, @Nonnull MutableSparseVector output) {
-        // Get the user's profile, which is a vector with their 'like' for each tag
-        SparseVector userVector = makeUserVector(user);
+	/**
+	 * Generate item scores personalized for a particular user.  For the TFIDF scorer, this will
+	 * prepare a user profile and compare it to item tag vectors to produce the score.
+	 *
+	 * @param user   The user to score for.
+	 * @param output The output vector.  The contract of this method is that the caller creates a
+	 *               vector whose possible keys are all items that should be scored; this method
+	 *               fills in the scores.
+	 */
+	@Override
+	public void score(long user, @Nonnull MutableSparseVector output) {
+		// Get the user's profile, which is a vector with their 'like' for each tag
+		SparseVector userVector = makeUserVector(user);
 
-        // Loop over each item requested and score it.
-        // The *domain* of the output vector is the items that we are to score.
-        for (VectorEntry e: output.fast(VectorEntry.State.EITHER)) {
-            // Score the item represented by 'e'.
-            // Get the item vector for this item
-            SparseVector iv = model.getItemVector(e.getKey());
-            // Compute the cosine of this item and the user's profile, store it in the output vector
-            double similarity = new CosineVectorSimilarity().similarity(iv, userVector);
-            output.set(e.getKey(), similarity);
-            
-            // And remove this exception to say you've implemented it
-            //throw new UnsupportedOperationException("stub implementation");
-        }
-    }
+		// Loop over each item requested and score it.
+		// The *domain* of the output vector is the items that we are to score.
+		for (VectorEntry e: output.fast(VectorEntry.State.EITHER)) {
+			// Score the item represented by 'e'.
+			// Get the item vector for this item
+			SparseVector iv = model.getItemVector(e.getKey());
+			// Compute the cosine of this item and the user's profile, store it in the output vector
+			double similarity = new CosineVectorSimilarity().similarity(iv, userVector);
+			output.set(e.getKey(), similarity);
 
-    private SparseVector makeUserVector(long user) {
-        // Get the user's ratings
-        List<Rating> userRatings = dao.getEventsForUser(user, Rating.class);
-        if (userRatings == null) {
-            // the user doesn't exist
-            return SparseVector.empty();
-        }
+			// And remove this exception to say you've implemented it
+			//throw new UnsupportedOperationException("stub implementation");
+		}
+	}
 
-        // Create a new vector over tags to accumulate the user profile
-        MutableSparseVector profile = model.newTagVector();
-        // Fill it with 0's initially - they don't like anything
-        profile.fill(0);
-        
+	@SuppressWarnings("unused")
+	private SparseVector makeUserVector_unweighted(long user) {
+		// Get the user's ratings
+		List<Rating> userRatings = dao.getEventsForUser(user, Rating.class);
+		if (userRatings == null) {
+			// the user doesn't exist
+			return SparseVector.empty();
+		}
 
-        // Iterate over the user's ratings to build their profile
-        for (Rating r: userRatings) {
-            // In LensKit, ratings are expressions of preference
-            Preference p = r.getPreference();
-            // We'll never have a null preference. But in LensKit, ratings can have null
-            // preferences to express the user unrating an item
-            if (p != null && p.getValue() >= 3.5) {
-                // The user likes this item!
-                // TODO Get the item's vector and add it to the user's profile
-            	long itemId = p.getItemId();
-            	SparseVector iv = model.getItemVector(itemId);
-            	profile.add(iv);
-            }
-        }
+		// Create a new vector over tags to accumulate the user profile
+		MutableSparseVector profile = model.newTagVector();
+		// Fill it with 0's initially - they don't like anything
+		profile.fill(0);
 
-        // The profile is accumulated, return it.
-        // It is good practice to return a frozen vector.
-        return profile.freeze();
-    }
-    
-//  double sumRatings = 0.0;
-//  int numRatings = 0;
-//  for( Rating r : userRatings){
-//  	Preference p = r.getPreference();
-//  	if (p != null){
-//  		sumRatings += p.getValue();
-//  		numRatings += 1;
-//  	}
-//  }
-//  double meanRatings = sumRatings/numRatings;
+		// Iterate over the user's ratings to build their profile
+		for (Rating r: userRatings) {
+			// In LensKit, ratings are expressions of preference
+			Preference p = r.getPreference();
+			// We'll never have a null preference. But in LensKit, ratings can have null
+			// preferences to express the user unrating an item
+			if (p != null && p.getValue() >= 3.5) {
+				// The user likes this item!
+				// Get the item's vector and add it to the user's profile
+				long itemId = p.getItemId();
+				SparseVector iv = model.getItemVector(itemId);
+				profile.add(iv);
+			}
+		}
+		// The profile is accumulated, return it.
+		// It is good practice to return a frozen vector.
+		return profile.freeze();
+	}
+
+	private SparseVector makeUserVector(long user) {
+		// Get the user's ratings
+		List<Rating> userRatings = dao.getEventsForUser(user, Rating.class);
+		if (userRatings == null) {
+			// the user doesn't exist
+			return SparseVector.empty();
+		}
+
+		double sumRatings = 0.0;
+		int numRatings = 0;
+		for( Rating r : userRatings){
+			Preference p = r.getPreference();
+			if (p != null){
+				sumRatings += p.getValue();
+				numRatings += 1;
+			}
+		}
+		double meanRatings = sumRatings/numRatings;
+
+		// Create a new vector over tags to accumulate the user profile
+		MutableSparseVector profile = model.newTagVector();
+		// Fill it with 0's initially - they don't like anything
+		profile.fill(0);
+
+		// Iterate over the user's ratings to build their profile
+		for (Rating r: userRatings) {
+			// In LensKit, ratings are expressions of preference
+			Preference p = r.getPreference();
+			// We'll never have a null preference. But in LensKit, ratings can have null
+			// preferences to express the user unrating an item
+			if (p != null) {
+				// weigth the item according to the user scores
+				// Get the item's vector and add it to the user's profile
+				long itemId = p.getItemId();
+				SparseVector iv = model.getItemVector(itemId);
+				MutableSparseVector mutableIv = iv.mutableCopy();
+				double weight = p.getValue() - meanRatings;
+				mutableIv.multiply(weight);
+				profile.add(mutableIv);
+			}
+		}
+		// The profile is accumulated, return it.
+		// It is good practice to return a frozen vector.
+		return profile.freeze();
+	}
 }
