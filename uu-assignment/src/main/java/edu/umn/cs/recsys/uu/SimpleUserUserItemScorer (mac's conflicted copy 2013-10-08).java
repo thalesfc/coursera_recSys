e173f38148ba102c722ab6012bc04b1f17b91633
Thales Costa <thalesfc@gmail.com>
2013-10-09 01:03:08 -0300

    Finish first version of UU collaborative recSys

--- uu-assignment/src/main/java/edu/umn/cs/recsys/uu/SimpleUserUserItemScorer (mac's conflicted copy 2013-10-08).java
+++ uu-assignment/src/main/java/edu/umn/cs/recsys/uu/SimpleUserUserItemScorer (mac's conflicted copy 2013-10-08).java
@@ -1,17 +1,12 @@
 package edu.umn.cs.recsys.uu;
 
+import it.unimi.dsi.fastutil.longs.LongArraySet;
 import it.unimi.dsi.fastutil.longs.LongSet;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.SortedMap;
 import java.util.TreeMap;
-
-import javax.annotation.Nonnull;
-import javax.inject.Inject;
 
 import org.grouplens.lenskit.basic.AbstractItemScorer;
 import org.grouplens.lenskit.data.dao.ItemEventDAO;
@@ -20,10 +15,14 @@ import org.grouplens.lenskit.data.event.Rating;
 import org.grouplens.lenskit.data.history.History;
 import org.grouplens.lenskit.data.history.RatingVectorUserHistorySummarizer;
 import org.grouplens.lenskit.data.history.UserHistory;
+import org.grouplens.lenskit.vectors.ImmutableSparseVector;
 import org.grouplens.lenskit.vectors.MutableSparseVector;
 import org.grouplens.lenskit.vectors.SparseVector;
 import org.grouplens.lenskit.vectors.VectorEntry;
 import org.grouplens.lenskit.vectors.similarity.CosineVectorSimilarity;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
 
 /**
  * User-user item scorer.
@@ -46,46 +45,23 @@ public class SimpleUserUserItemScorer extends AbstractItemScorer {
         // This is the loop structure to iterate over items to score
         for (VectorEntry e: scores.fast(VectorEntry.State.EITHER)) {
         	
-        	Long item = e.getKey();
         	// get the 30 most similar users who have rated the item
-        	List<Entry<Double, Long>> neighbors = similarUsers(user, item);
-//        	System.err.println("# Neighboors of user " + user + " - item: " + item);
-//        	System.err.println(neighbors);
-        	
-        	double mu = userVector.mean();
-        	double downPart = 0.0;
-        	double upperPart = 0.0;
-        	
-        	// iterate through all neighbors
-        	for (Entry<Double, Long> entry : neighbors) {
-        		double s_uv = entry.getKey();
-        		long neighId = entry.getValue();
-        		
-        		SparseVector nrv = getUserRatingVector(neighId);
-        		double mu_v = nrv.mean();
-        		double r_vi = nrv.get(item);
-        		
-        		upperPart += s_uv * (r_vi - mu_v);
-        		downPart += Math.abs(s_uv);
-			}
-        	
-        	double p_ui = mu + upperPart / downPart; 
-        	scores.set(item, p_ui);
+        	LongSet neighbors = similarUsers(user, e.getKey());
         }
     }
 
 
-	private List<Entry<Double, Long>> similarUsers(long activeUser, long item) {
+	private LongSet similarUsers(long activeUser, long item) {
 		
 		// the mean-centered rating vector for the active user
 		MutableSparseVector amrv = getUserMeanCenteredRatingVector(activeUser);
 		
-		SortedMap<Double, Long> neighborhood = new TreeMap<Double, Long>(Collections.reverseOrder()); 
+		SortedMap<Double, Long> neighborhood = new TreeMap<Double, Long>(); 
 		
 
 		
 		// all users who rated the given item
-//		System.err.println("# getting users who rated item: " + item);
+		System.err.println("# getting users who rated item: " + item);
 		LongSet possibleNeighbors = itemDao.getUsersForItem(item);
 		for (Long user : possibleNeighbors) {
 			if(user == activeUser) continue;
@@ -97,13 +73,16 @@ public class SimpleUserUserItemScorer extends AbstractItemScorer {
 		}
 		
 		// return the top 30 similar users who rated the given item
-		List<Entry<Double, Long>> returned = new ArrayList<Map.Entry<Double,Long>>(30);
-		int count = 0;
+		LongSet returned = new LongArraySet(30);
 		for(Map.Entry<Double, Long> entry : neighborhood.entrySet()){
-			returned.add(entry);
-			if(++count == 30) break;
+			
 		}
-		return returned;
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	/**
