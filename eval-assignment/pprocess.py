183e3a7e6660198a7c3941fdcb9ffd775cb64f74
Thales Costa <thalesfc@gmail.com>
2013-11-04 18:36:55 -0200

    Compute user-mean RMSE considering the folds

--- eval-assignment/pprocess.py
+++ eval-assignment/pprocess.py
@@ -1,30 +1,25 @@
 import pandas
+import numpy
 import matplotlib.pyplot as plt
 
 udata = pandas.read_csv('target/analysis/eval-user.csv', header = 0, index_col = 0)
 
-# getting the mean per-user RMSE (RMSE.ByUser) for each algorithm
-PM_RMSE = udata.loc['PersMean'].mean()['RMSE']
-IM_RMSE = udata.loc['ItemMean'].mean()['RMSE']
-GB_RMSE = udata.loc['GlobalMean'].mean()['RMSE']
+algorithms = ['PersMean', 'ItemMean', 'GlobalMean']
+x =[]
 
-print 'PersMean', PM_RMSE
-print 'ItemMean', IM_RMSE
-print 'GlobalMean', GB_RMSE
+# for each algorithm
+for algo in algorithms:
+    #values array stores the user-mean RMSE of each algo.
+    values = []
+    # for each fold
+    for fold in range(5):
+        values.append(udata[udata['Partition'] == fold].loc[algo]['RMSE'].mean())
+    # compute the mean
+    print values
+    me = numpy.mean(values)
+    print me
+    x.append(me)
 
-x = [PM_RMSE, GB_RMSE, IM_RMSE]
-y = [2, 0, 1]
-#print x, y
-#y_label = ['PersMean', 'GlobalMean', 'ItemMean']
-#plt.scatter(x, y)
-#plt.savefig('uRMSE.pdf')
-
-POP_TnDCG = udata.loc['Popular']['TopN.nDCG'].mean()
-PM_TnDCG = udata.loc['PersMean']['TopN.nDCG'].mean()
-IM_TnDCG = udata.loc['ItemMean']['TopN.nDCG'].mean()
-
-x = [POP_TnDCG, PM_TnDCG, IM_TnDCG]
-print x
-plt.scatter(x, [0, 1, 2])
-plt.savefig('TOPnDCG.pdf')
-plt.show()
+y = [2, 1, 0]
+plt.scatter(x, y)
+plt.savefig('uRMSE.pdf')
