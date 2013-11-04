30f62fa8c7813c7880a6674ef9e3b6392a038e60
Thales Costa <thalesfc@gmail.com>
2013-11-04 18:51:25 -0200

    Final version of Pandas

--- eval-assignment/pprocess.py
+++ eval-assignment/pprocess.py
@@ -4,22 +4,34 @@ import matplotlib.pyplot as plt
 
 udata = pandas.read_csv('target/analysis/eval-user.csv', header = 0, index_col = 0)
 
+#########################################
+def operate(algorithms, field_name):
+    x = []
+    # for each algorithm
+    for algo in algorithms:
+        #values array stores the user-mean RMSE of each algo.
+        values = []
+        # for each fold
+        for fold in range(5):
+            values.append(udata[udata['Partition'] == fold].loc[algo][field_name].mean())
+        # compute the mean
+        me = numpy.mean(values)
+        x.append(me)
+    return x
+#########################################
+
+#############
+### user-mean RMSE
 algorithms = ['PersMean', 'ItemMean', 'GlobalMean']
-x =[]
-
-# for each algorithm
-for algo in algorithms:
-    #values array stores the user-mean RMSE of each algo.
-    values = []
-    # for each fold
-    for fold in range(5):
-        values.append(udata[udata['Partition'] == fold].loc[algo]['RMSE'].mean())
-    # compute the mean
-    print values
-    me = numpy.mean(values)
-    print me
-    x.append(me)
-
+x = operate(algorithms, 'RMSE') 
 y = [2, 1, 0]
 plt.scatter(x, y)
 plt.savefig('uRMSE.pdf')
+
+########################
+# top nDCG
+plt.clf()
+algorithms = ['Popular', 'PersMean', 'ItemMean']
+x = operate(algorithms, 'TopN.nDCG')
+plt.scatter(x, y)
+plt.savefig('TOPnDCG.pdf')
