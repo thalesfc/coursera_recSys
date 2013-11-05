6bdd782c73a19b9047275c5232a61ea8ce7d0b2c
Thales Costa <thalesfc@gmail.com>
2013-11-05 14:35:56 -0200

    First version of entropy metric

--- eval-assignment/src/eval/eval.groovy
+++ eval-assignment/src/eval/eval.groovy
@@ -18,77 +18,140 @@ import org.grouplens.lenskit.vectors.similarity.*
 // needed for both some algorithms and for metrics
 // this defines a variable containing a Groovy closure, if you care about that kind of thing
 tagConfig = {
-    bind ItemDAO to CSVItemTagDAO
-    set TagFile to new File("${project.config.dataDir}/movie-tags.csv")
-    set TitleFile to new File("${project.config.dataDir}/movie-titles.csv")
-    // need tag vocab & item DAO to be roots for diversity metric to use them
-    config.addRoot ItemTagDAO
-    config.addRoot TagVocabulary
+	bind ItemDAO to CSVItemTagDAO
+	set TagFile to new File("${project.config.dataDir}/movie-tags.csv")
+	set TitleFile to new File("${project.config.dataDir}/movie-titles.csv")
+	// need tag vocab & item DAO to be roots for diversity metric to use them
+	config.addRoot ItemTagDAO
+	config.addRoot TagVocabulary
 }
 
 // Run a train-test evaluation
 trainTest {
-    dataset crossfold("RecSysMOOC") {
-        source csvfile("MOOCRatings") {
-            file "${project.config.dataDir}/ratings.csv"
-            delimiter ","
-            domain {
-                minimum 0.5
-                maximum 5.0
-                precision 0.5
-            }
-        }
-        test "target/crossfold-5/test.%d.csv"
-        train "target/crossfold-5/train.%d.csv"
+	dataset crossfold("RecSysMOOC") {
+		source csvfile("MOOCRatings") {
+			file "${project.config.dataDir}/ratings.csv"
+			delimiter ","
+			domain {
+				minimum 0.5
+				maximum 5.0
+				precision 0.5
+			}
+		}
+		test "target/crossfold-5/test.%d.csv"
+		train "target/crossfold-5/train.%d.csv"
 
-        // hold out 5 random items from each user
-        order RandomOrder
-        holdout 5
+		// hold out 5 random items from each user
+		order RandomOrder
+		holdout 5
 
-        // split users into 5 sets
-        partitions 5
-    }
+		// split users into 5 sets
+		partitions 5
+	}
 
-    // Three different types of output for analysis.
-    output "${project.config.analysisDir}/eval-results.csv"
-    userOutput "${project.config.analysisDir}/eval-user.csv"
+	// Three different types of output for analysis.
+	output "${project.config.analysisDir}/eval-results.csv"
+	userOutput "${project.config.analysisDir}/eval-user.csv"
 
-    metric CoveragePredictMetric
-    metric RMSEPredictMetric
-    metric NDCGPredictMetric
+	metric CoveragePredictMetric
+	metric RMSEPredictMetric
+	metric NDCGPredictMetric
 
-    // Compute nDCG trying to recommend lists of 10 from all items
-    // This suffers from similar problems as the unary ratings case!
-    metric topNnDCG {
-        candidates ItemSelectors.allItems()
-        exclude ItemSelectors.trainingItems()
-        listSize 10
-    }
-    // measure the entropy of the top 10 items
-    metric new TagEntropyMetric(10)
+	// Compute nDCG trying to recommend lists of 10 from all items
+	// This suffers from similar problems as the unary ratings case!
+	metric topNnDCG {
+		candidates ItemSelectors.allItems()
+		exclude ItemSelectors.trainingItems()
+		listSize 10
+	}
+	// measure the entropy of the top 10 items
+	metric new TagEntropyMetric(10)
 
-    algorithm("GlobalMean") {
-        include tagConfig
-        // score items by the global mean
-        bind ItemScorer to GlobalMeanRatingItemScorer
-        // recommendation is meaningless for this algorithm
-        bind ItemRecommender to null
-    }
-    algorithm("Popular") {
-        include tagConfig
-        // score items by their popularity
-        bind ItemScorer to PopularityItemScorer
-        // rating prediction is meaningless for this algorithm
-        bind RatingPredictor to null
-    }
-    algorithm("ItemMean") {
-        include tagConfig
-        // score items by their mean rating
-        bind ItemScorer to ItemMeanRatingItemScorer
-    }
-    algorithm("PersMean") {
-        include tagConfig
-        bind ItemScorer to UserMeanItemScorer
-        bind (UserMeanBaseline, ItemScorer) to ItemMeanRatingItemScorer
-    }
+	algorithm("GlobalMean") {
+		include tagConfig
+		// score items by the global mean
+		bind ItemScorer to GlobalMeanRatingItemScorer
+		// recommendation is meaningless for this algorithm
+		bind ItemRecommender to null
+	}
+	algorithm("Popular") {
+		include tagConfig
+		// score items by their popularity
+		bind ItemScorer to PopularityItemScorer
+		// rating prediction is meaningless for this algorithm
+		bind RatingPredictor to null
+	}
+	algorithm("ItemMean") {
+		include tagConfig
+		// score items by their mean rating
+		bind ItemScorer to ItemMeanRatingItemScorer
+	}
+	algorithm("PersMean") {
+		include tagConfig
+		bind ItemScorer to UserMeanItemScorer
+		bind (UserMeanBaseline, ItemScorer) to ItemMeanRatingItemScorer
+	}
+
+	//add the user-user collaborative filter and tune its neighborhood size and normalization
+	for (nnbrs in [5, 10, 15, 20, 25, 30, 40, 50, 75, 100]) {
+		algorithm("UserUser") {
+			include tagConfig
+			// Attributes let you specify additional properties of the algorithm.
+			// They go in the output file, so you can do things like plot accuracy by neighborhood size
+			attributes["NNbrs"] = nnbrs
+			// use the user-user rating predictor
+			bind ItemScorer to UserUserItemScorer
+			set NeighborhoodSize to nnbrs
+			bind VectorSimilarity to PearsonCorrelation
+		}
+
+		algorithm("UserUserNorm") {
+			include tagConfig
+			// Attributes let you specify additional properties of the algorithm.
+			// They go in the output file, so you can do things like plot accuracy by neighborhood size
+			attributes["NNbrs"] = nnbrs
+			// use the user-user rating predictor
+			bind ItemScorer to UserUserItemScorer
+			set NeighborhoodSize to nnbrs
+			bind VectorNormalizer to MeanCenteringVectorNormalizer
+			bind VectorSimilarity to PearsonCorrelation
+		}
+
+		// using cosine similarity instead of Pearson correlation
+		algorithm("UserUserCosine") {
+			include tagConfig
+			// Attributes let you specify additional properties of the algorithm.
+			// They go in the output file, so you can do things like plot accuracy by neighborhood size
+			attributes["NNbrs"] = nnbrs
+			// use the user-user rating predictor
+			bind ItemScorer to UserUserItemScorer
+			set NeighborhoodSize to nnbrs
+			bind VectorNormalizer to MeanCenteringVectorNormalizer
+			bind VectorSimilarity to CosineVectorSimilarity
+		}
+
+		algorithm("Lucene") {
+			attributes["NNbrs"] = nnbrs
+			include tagConfig
+			bind ItemScorer to ItemItemScorer
+			bind ItemItemModel to LuceneItemItemModel
+			set NeighborhoodSize to nnbrs
+			// consider using all 100 movies as neighbors
+			set ModelSize to 100
+		}
+
+		algorithm("LuceneNorm") {
+			attributes["NNbrs"] = nnbrs
+			include tagConfig
+			bind ItemScorer to ItemItemScorer
+			bind ItemItemModel to LuceneItemItemModel
+			set NeighborhoodSize to nnbrs
+			// consider using all 100 movies as neighbors
+			set ModelSize to 100
+			bind UserVectorNormalizer to BaselineSubtractingUserVectorNormalizer
+			within (UserVectorNormalizer) {
+				bind (BaselineScorer, ItemScorer) to ItemMeanRatingItemScorer
+			}
+		}
+	}
 }
--- eval-assignment/src/main/java/edu/umn/cs/recsys/TagEntropyMetric.java
+++ eval-assignment/src/main/java/edu/umn/cs/recsys/TagEntropyMetric.java
@@ -1,7 +1,14 @@
 package edu.umn.cs.recsys;
 
-import com.google.common.collect.ImmutableList;
-import edu.umn.cs.recsys.dao.ItemTagDAO;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+
 import org.grouplens.lenskit.core.LenskitRecommender;
 import org.grouplens.lenskit.eval.algorithm.AlgorithmInstance;
 import org.grouplens.lenskit.eval.data.traintest.TTDataSet;
@@ -10,107 +17,143 @@ import org.grouplens.lenskit.eval.metrics.TestUserMetricAccumulator;
 import org.grouplens.lenskit.eval.metrics.topn.ItemSelectors;
 import org.grouplens.lenskit.eval.traintest.TestUser;
 import org.grouplens.lenskit.scored.ScoredId;
-import org.grouplens.lenskit.vectors.MutableSparseVector;
-import org.grouplens.lenskit.vectors.VectorEntry;
 
-import javax.annotation.Nonnull;
-import java.util.List;
+import com.google.common.collect.ImmutableList;
+
+import edu.umn.cs.recsys.dao.ItemTagDAO;
 
 /**
  * A metric that measures the tag entropy of the recommended items.
  * @author <a href="http://www.grouplens.org">GroupLens Research</a>
  */
 public class TagEntropyMetric extends AbstractTestUserMetric {
-    private final int listSize;
-    private final List<String> columns;
+	private final int listSize;
+	private final List<String> columns;
 
-    /**
-     * Construct a new tag entropy metric.
-     * 
-     * @param nitems The number of items to request.
-     */
-    public TagEntropyMetric(int nitems) {
-        listSize = nitems;
-        // initialize column labels with list length
-        columns = ImmutableList.of(String.format("TagEntropy@%d", nitems));
-    }
+	/**
+	 * Construct a new tag entropy metric.
+	 * 
+	 * @param nitems The number of items to request.
+	 */
+	public TagEntropyMetric(int nitems) {
+		listSize = nitems;
+		// initialize column labels with list length
+		columns = ImmutableList.of(String.format("TagEntropy@%d", nitems));
+	}
 
-    /**
-     * Make a metric accumulator.  Metrics operate with <em>accumulators</em>, which are created
-     * for each algorithm and data set.  The accumulator measures each user's output, and
-     * accumulates the results into a global statistic for the whole evaluation.
-     *
-     * @param algorithm The algorithm being tested.
-     * @param data The data set being tested with.
-     * @return An accumulator for analyzing this algorithm and data set.
-     */
-    @Override
-    public TestUserMetricAccumulator makeAccumulator(AlgorithmInstance algorithm, TTDataSet data) {
-        return new TagEntropyAccumulator();
-    }
+	/**
+	 * Make a metric accumulator.  Metrics operate with <em>accumulators</em>, which are created
+	 * for each algorithm and data set.  The accumulator measures each user's output, and
+	 * accumulates the results into a global statistic for the whole evaluation.
+	 *
+	 * @param algorithm The algorithm being tested.
+	 * @param data The data set being tested with.
+	 * @return An accumulator for analyzing this algorithm and data set.
+	 */
+	@Override
+	public TestUserMetricAccumulator makeAccumulator(AlgorithmInstance algorithm, TTDataSet data) {
+		return new TagEntropyAccumulator();
+	}
 
-    /**
-     * Return the labels for the (global) columns returned by this metric.
-     * @return The labels for the global columns.
-     */
-    @Override
-    public List<String> getColumnLabels() {
-        return columns;
-    }
+	/**
+	 * Return the labels for the (global) columns returned by this metric.
+	 * @return The labels for the global columns.
+	 */
+	@Override
+	public List<String> getColumnLabels() {
+		return columns;
+	}
 
-    /**
-     * Return the lables for the per-user columns returned by this metric.
-     */
-    @Override
-    public List<String> getUserColumnLabels() {
-        // per-user and global have the same fields, they just differ in aggregation.
-        return columns;
-    }
+	/**
+	 * Return the lables for the per-user columns returned by this metric.
+	 */
+	@Override
+	public List<String> getUserColumnLabels() {
+		// per-user and global have the same fields, they just differ in aggregation.
+		return columns;
+	}
 
 
-    private class TagEntropyAccumulator implements TestUserMetricAccumulator {
-        private double totalEntropy = 0;
-        private int userCount = 0;
+	private class TagEntropyAccumulator implements TestUserMetricAccumulator {
+		private double totalEntropy = 0;
+		private int userCount = 0;
 
-        /**
-         * Evaluate a single test user's recommendations or predictions.
-         * @param testUser The user's recommendation result.
-         * @return The values for the per-user columns.
-         */
-        @Nonnull
-        @Override
-        public Object[] evaluate(TestUser testUser) {
-            List<ScoredId> recommendations =
-                    testUser.getRecommendations(listSize,
-                                                ItemSelectors.allItems(),
-                                                ItemSelectors.trainingItems());
-            if (recommendations == null) {
-                return new Object[1];
-            }
-            LenskitRecommender lkrec = (LenskitRecommender) testUser.getRecommender();
-            ItemTagDAO tagDAO = lkrec.get(ItemTagDAO.class);
-            TagVocabulary vocab = lkrec.get(TagVocabulary.class);
+		/**
+		 * Evaluate a single test user's recommendations or predictions.
+		 * @param testUser The user's recommendation result.
+		 * @return The values for the per-user columns.
+		 */
+		@Nonnull
+		@Override
+		public Object[] evaluate(TestUser testUser) {
+			List<ScoredId> recommendations =
+					testUser.getRecommendations(listSize,
+							ItemSelectors.allItems(),
+							ItemSelectors.trainingItems());
+			if (recommendations == null) {
+				return new Object[1];
+			}
+			LenskitRecommender lkrec = (LenskitRecommender) testUser.getRecommender();
+			ItemTagDAO tagDAO = lkrec.get(ItemTagDAO.class);
+			//TagVocabulary vocab = lkrec.get(TagVocabulary.class);
 
-            double entropy = 0;
+			double entropy = 0;
 
-            // TODO Implement the entropy metric
+			/*
+			 * Creating the uniqueCaseInsensitiveTagsInRecommendationListForThisUser
+			 */
+			// unique Case Insensitive Tags In Recommendation List For This User
+			Set<String> uniqTags = new HashSet<String>();
+			// {movie_id : set(movie_tags)}
+			Map<Long, Set<String>> moviesTags = new HashMap<Long, Set<String>>(recommendations.size());
+			for (ScoredId movie : recommendations) {
+				Set<String> movieTags = new HashSet<String>();
+				List<String> tags = tagDAO.getItemTags(movie.getId());
+				for(String tag : tags){
+					String normalized = tag.toLowerCase();
+					movieTags.add(normalized);
+				}
+				moviesTags.put(movie.getId(), movieTags);
+				uniqTags.addAll(movieTags);
+			}
 
-            totalEntropy += entropy;
-            userCount += 1;
-            return new Object[]{entropy};
-        }
+			/*
+			 * # Unique Case Insensitive Tags For This Movie
+			 */
+			double numberOfUniqueCaseInsensitiveTagsForThisMovie = uniqTags.size();
+			double numberOfMoviesInTheRecommendationList = recommendations.size();
 
-        /**
-         * Get the final aggregate results.  This is called after all users have been evaluated, and
-         * returns the values for the columns in the global output.
-         *
-         * @return The final, aggregated columns.
-         */
-        @Nonnull
-        @Override
-        public Object[] finalResults() {
-            // return a single field, the average entropy
-            return new Object[]{totalEntropy / userCount};
-        }
-    }
+			/*
+			 * calculating entropy
+			 */
+			for(String tag : uniqTags){
+				double Pt_given_List = 0.0;
+				for(Entry<Long, Set<String>> entry : moviesTags.entrySet()){
+					Set<String> mTags = entry.getValue();
+					if(mTags.contains(tag)){
+						Pt_given_List += 1.0/numberOfUniqueCaseInsensitiveTagsForThisMovie;
+					}
+				}
+				Pt_given_List /= numberOfMoviesInTheRecommendationList;
+				entropy -= Pt_given_List*Math.log(Pt_given_List); //entropy in nats
+			}
+			entropy /= Math.log(2.0); //entropy in bits 
+			totalEntropy += entropy; //overall entropy for all users on this partition 
+			userCount += 1; //number of users for averaging the entropy for this partition
+			return new Object[]{entropy};
+		}
+
+		/**
+		 * Get the final aggregate results.  This is called after all users have been evaluated, and
+		 * returns the values for the columns in the global output.
+		 *
+		 * @return The final, aggregated columns.
+		 */
+		@Nonnull
+		@Override
+		public Object[] finalResults() {
+			// return a single field, the average entropy
+			return new Object[]{totalEntropy / userCount};
+		}
+	}
 }
