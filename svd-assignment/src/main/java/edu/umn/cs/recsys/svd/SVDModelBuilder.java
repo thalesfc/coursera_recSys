baba0ad0c743552ecf9220e9d834c67dc68ca2f9
Thales Costa <thalesfc@gmail.com>
2013-12-01 22:14:11 -0200

    Implement SVD model builder

--- svd-assignment/src/main/java/edu/umn/cs/recsys/svd/SVDModelBuilder.java
+++ svd-assignment/src/main/java/edu/umn/cs/recsys/svd/SVDModelBuilder.java
@@ -27,96 +27,109 @@ import javax.inject.Provider;
  * Model builder that computes the SVD model.
  */
 public class SVDModelBuilder implements Provider<SVDModel> {
-    private static final Logger logger = LoggerFactory.getLogger(SVDModelBuilder.class);
+	private static final Logger logger = LoggerFactory.getLogger(SVDModelBuilder.class);
 
-    private final UserEventDAO userEventDAO;
-    private final UserDAO userDAO;
-    private final ItemDAO itemDAO;
-    private final ItemScorer baselineScorer;
-    private final int featureCount;
+	private final UserEventDAO userEventDAO;
+	private final UserDAO userDAO;
+	private final ItemDAO itemDAO;
+	private final ItemScorer baselineScorer;
+	private final int featureCount;
 
-    /**
-     * Construct the model builder.
-     * @param uedao The user event DAO.
-     * @param udao The user DAO.
-     * @param idao The item DAO.
-     * @param baseline The baseline scorer (this will be used to compute means).
-     * @param nfeatures The number of latent features to train.
-     */
-    @Inject
-    public SVDModelBuilder(@Transient UserEventDAO uedao,
-                           @Transient UserDAO udao,
-                           @Transient ItemDAO idao,
-                           @Transient @BaselineScorer ItemScorer baseline,
-                           @LatentFeatureCount int nfeatures) {
-        logger.debug("user DAO: {}", udao);
-        userEventDAO = uedao;
-        userDAO = udao;
-        itemDAO = idao;
-        baselineScorer = baseline;
-        featureCount = nfeatures;
-    }
+	/**
+	 * Construct the model builder.
+	 * @param uedao The user event DAO.
+	 * @param udao The user DAO.
+	 * @param idao The item DAO.
+	 * @param baseline The baseline scorer (this will be used to compute means).
+	 * @param nfeatures The number of latent features to train.
+	 */
+	@Inject
+	public SVDModelBuilder(@Transient UserEventDAO uedao,
+			@Transient UserDAO udao,
+			@Transient ItemDAO idao,
+			@Transient @BaselineScorer ItemScorer baseline,
+			@LatentFeatureCount int nfeatures) {
+		logger.debug("user DAO: {}", udao);
+		userEventDAO = uedao;
+		userDAO = udao;
+		itemDAO = idao;
+		baselineScorer = baseline;
+		featureCount = nfeatures;
+	}
 
-    /**
-     * Build the SVD model.
-     *
-     * @return A singular value decomposition recommender model.
-     */
-    @Override
-    public SVDModel get() {
-        // Create index mappings of user and item IDs.
-        // You can use these to find row and columns in the matrix based on user/item IDs.
-        IdIndexMapping userMapping = IdIndexMapping.create(userDAO.getUserIds());
-        logger.debug("indexed {} users", userMapping.size());
-        IdIndexMapping itemMapping = IdIndexMapping.create(itemDAO.getItemIds());
-        logger.debug("indexed {} items", itemMapping.size());
+	/**
+	 * Build the SVD model.
+	 *
+	 * @return A singular value decomposition recommender model.
+	 */
+	@Override
+	public SVDModel get() {
+		// Create index mappings of user and item IDs.
+		// You can use these to find row and columns in the matrix based on user/item IDs.
+		IdIndexMapping userMapping = IdIndexMapping.create(userDAO.getUserIds());
+		logger.debug("indexed {} users", userMapping.size());
+		IdIndexMapping itemMapping = IdIndexMapping.create(itemDAO.getItemIds());
+		logger.debug("indexed {} items", itemMapping.size());
 
-        // We have to do 2 things:
-        // First, prepare a matrix containing the rating data.
-        RealMatrix matrix = createRatingMatrix(userMapping, itemMapping);
+		// We have to do 2 things:
+		// First, prepare a matrix containing the rating data.
+		RealMatrix matrix = createRatingMatrix(userMapping, itemMapping);
 
-        // Second, compute its factorization
-        // All the work is done in the constructor
-        SingularValueDecomposition svd = new SingularValueDecomposition(matrix);
+		// Second, compute its factorization
+		// All the work is done in the constructor
+		SingularValueDecomposition svd = new SingularValueDecomposition(matrix);
 
-        // Third, truncate the decomposed matrix
-        // TODO Truncate the matrices and construct the SVD model
+		// Third, truncate the decomposed matrix
+		// Truncate the matrices and construct the SVD model
+		RealMatrix U = svd.getU().getSubMatrix(0, svd.getU().getRowDimension() - 1, 0, featureCount - 1);
+		RealMatrix S = svd.getS().getSubMatrix(0, featureCount - 1, 0, featureCount - 1);
+		RealMatrix V = svd.getV().getSubMatrix(0, svd.getV().getRowDimension() - 1, 0, featureCount -1);
+		
+		// Replace this throw line with returning the model when you are finished
+//		throw new UnsupportedOperationException("SVD model not yet implemented");
+		return new SVDModel(userMapping, userMapping, U, V, S);
+	}
 
-        // TODO Replace this throw line with returning the model when you are finished
-        throw new UnsupportedOperationException("SVD model not yet implemented");
-    }
+	/**
+	 * Build a rating matrix from the rating data.  Each user's ratings are first normalized
+	 * by subtracting a baseline score (usually a mean).
+	 *
+	 * @param userMapping The index mapping of user IDs to column numbers.
+	 * @param itemMapping The index mapping of item IDs to row numbers.
+	 * @return A matrix storing the <i>normalized</i> user ratings.
+	 */
+	private RealMatrix createRatingMatrix(IdIndexMapping userMapping, IdIndexMapping itemMapping) {
+		final int nusers = userMapping.size();
+		final int nitems = itemMapping.size();
 
-    /**
-     * Build a rating matrix from the rating data.  Each user's ratings are first normalized
-     * by subtracting a baseline score (usually a mean).
-     *
-     * @param userMapping The index mapping of user IDs to column numbers.
-     * @param itemMapping The index mapping of item IDs to row numbers.
-     * @return A matrix storing the <i>normalized</i> user ratings.
-     */
-    private RealMatrix createRatingMatrix(IdIndexMapping userMapping, IdIndexMapping itemMapping) {
-        final int nusers = userMapping.size();
-        final int nitems = itemMapping.size();
+		// Create a matrix with users on rows and items on columns
+		logger.info("creating {} by {} rating matrix", nusers, nitems);
+		RealMatrix matrix = MatrixUtils.createRealMatrix(nusers, nitems);
 
-        // Create a matrix with users on rows and items on columns
-        logger.info("creating {} by {} rating matrix", nusers, nitems);
-        RealMatrix matrix = MatrixUtils.createRealMatrix(nusers, nitems);
+		// populate it with data
+		Cursor<UserHistory<Event>> users = userEventDAO.streamEventsByUser();
+		try {
+			for (UserHistory<Event> user: users) {
+				// Get the row number for this user
+				int u = userMapping.getIndex(user.getUserId());
+				MutableSparseVector ratings = Ratings.userRatingVector(user.filter(Rating.class));
+				MutableSparseVector baselines = MutableSparseVector.create(ratings.keySet());
+				baselineScorer.score(user.getUserId(), baselines);
 
-        // populate it with data
-        Cursor<UserHistory<Event>> users = userEventDAO.streamEventsByUser();
-        try {
-            for (UserHistory<Event> user: users) {
-                // Get the row number for this user
-                int u = userMapping.getIndex(user.getUserId());
-                MutableSparseVector ratings = Ratings.userRatingVector(user.filter(Rating.class));
-                MutableSparseVector baselines = MutableSparseVector.create(ratings.keySet());
-                baselineScorer.score(user.getUserId(), baselines);
-                // TODO Populate this user's row with their ratings, minus the baseline scores
-            }
-        } finally {
-            users.close();
-        }
+				// Populate this user's row with their ratings, minus the baseline scores
+				ratings.subtract(baselines); // v = r - baseline
 
-        return matrix;
-    }
+				for (VectorEntry vectorEntry : ratings.fast(VectorEntry.State.EITHER)) {
+					int i = itemMapping.getIndex(vectorEntry.getKey());
+					double v = vectorEntry.getValue();
+					matrix.setEntry(u, i, v);
+				}
+
+			}
+		} finally {
+			users.close();
+		}
+
+		return matrix;
+	}
 }
